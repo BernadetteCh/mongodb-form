f7d1390b3b2a461b7f4a7f3d746888aab2c1c4a2
Bernadette Christiner <b.christiner93@gmail.com>
2022-11-18 21:40:46 +0100

    Implement Edit component

--- client/my-app/src/components/Edit.js
+++ client/my-app/src/components/Edit.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
+import User from "../components/User";
+import "../App.css";
 
 function Edit({ renderPage }) {
+  const [apiUrl] = useState("http://localhost:8000/data");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const data = await fetch(apiUrl);
+      const response = await data.json();
+
+      setData(response);
+    }
+    getData();
+  }, [apiUrl]);
+
   const changePage = () => {
     renderPage(false);
   };
 
+  const deleteRequest = async (id) => {
+    console.log(id);
+    await fetch(`http://localhost:8000/data/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  };
+
   return (
-    <div>
-      {" "}
-      <Button onClick={changePage}>Form</Button>
+    <div className="user-table">
+      <table>
+        <thead>
+          <tr>
+            <th>FirstName</th>
+            <th>SecondName</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        {data.map((user, index) => {
+          return (
+            <User
+              user={user}
+              key={index}
+              myKey={index}
+              deleteRequest={deleteRequest}
+            />
+          );
+        })}
+      </table>
+
+      <Button onClick={changePage} className="mt-5">
+        Go Back To Form
+      </Button>
     </div>
   );
 }
