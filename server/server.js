79cfc09f0bd1a9989321c91f3d7ea882c46b863f
Bernadette Christiner <b.christiner93@gmail.com>
2022-11-19 17:22:21 +0100

    Change post request to save new user

--- server/server.js
+++ server/server.js
@@ -5,21 +5,23 @@ const port = 8000;
 const mongoose = require("mongoose");
 const db = mongoose.connection;
 mongoose.connect("mongodb://localhost/newsLetterUsers");
-const user = require("./UserSchema");
+const User = require("./UserSchema");
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/userData", async (req, response) => {
-  const newUser = {
+  let newsLetterUser = new User({
     firstName: req.body.user.firstName,
     secondName: req.body.user.secondName,
     email: req.body.user.email,
-  };
-  db.collection("users").insertOne(newUser, function (err, res) {
-    if (err) throw err;
   });
+  try {
+    newsLetterUser = await newsLetterUser.save();
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 app.get("/data", (req, res) => {
